5e48f6835b55d86240e40e1f0c163fc3a6e83a90
Matouš Vrba <vrbamato@fel.cvut.cz>
2023-01-20 14:33:43 +0100

    added the jsoncpp library as a submodule and the corresponding methods for parsing ROS2 messages from json

--- dynmsg/src/msg_parser_json_cpp.cpp
+++ dynmsg/src/msg_parser_json_cpp.cpp
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <yaml-cpp/yaml.h>
+#include <json/json.h>
 
 #include <iostream>
 #include <string>
@@ -30,7 +30,7 @@
 #include "rcutils/allocator.h"
 
 #include "dynmsg/config.hpp"
-#include "dynmsg/msg_parser_json.hpp"
+#include "dynmsg/msg_parser.hpp"
 #include "dynmsg/string_utils.hpp"
 
 namespace dynmsg
@@ -41,8 +41,8 @@ namespace cpp
 namespace impl
 {
 
-void yaml_to_rosmsg_impl(
-  const YAML::Node & root,
+void json_to_rosmsg_impl(
+  const Json::Value & root,
   const TypeInfo_Cpp * typeinfo,
   uint8_t * buffer
 );
@@ -173,198 +173,198 @@ struct TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_WSTRING>
 // Write an individual member into the binary message - generic
 template<int RosTypeId>
 void write_member_item(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_member_item<T>(): " << std::endl);
   using CppType = typename TypeMappingCpp<RosTypeId>::CppType;
-  *reinterpret_cast<CppType *>(buffer) = yaml.as<CppType>();
+  *reinterpret_cast<CppType *>(buffer) = json.as<CppType>();
 }
 
-#ifdef DYNMSG_YAML_CPP_BAD_INT8_HANDLING
+#ifdef DYNMSG_json_CPP_BAD_INT8_HANDLING
 // Write an individual member into the binary message - [u]int8_t (char, octet, uint8, int8)
 template<>
 void write_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_CHAR>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_member_item<char>(): " << std::endl);
   using CppType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_CHAR>::CppType;
-  std::string s = yaml.as<std::string>();
+  std::string s = json.as<std::string>();
   *reinterpret_cast<CppType *>(buffer) = (uint8_t)std::stoul(s);
 }
 template<>
 void write_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_OCTET>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_member_item<octet>(): " << std::endl);
   using CppType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_OCTET>::CppType;
-  std::string s = yaml.as<std::string>();
+  std::string s = json.as<std::string>();
   *reinterpret_cast<CppType *>(buffer) = (uint8_t)std::stoul(s);
 }
 template<>
 void write_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_UINT8>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_member_item<uint8>(): " << std::endl);
   using CppType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_UINT8>::CppType;
-  std::string s = yaml.as<std::string>();
+  std::string s = json.as<std::string>();
   *reinterpret_cast<CppType *>(buffer) = (uint8_t)std::stoul(s);
 }
 template<>
 void write_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_INT8>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_member_item<int8>(): " << std::endl);
   using CppType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_INT8>::CppType;
-  std::string s = yaml.as<std::string>();
+  std::string s = json.as<std::string>();
   *reinterpret_cast<CppType *>(buffer) = (int8_t)std::stoi(s);
 }
-#endif  // DYNMSG_YAML_CPP_BAD_INT8_HANDLING
+#endif  // DYNMSG_json_CPP_BAD_INT8_HANDLING
 
 // Write an individual member into the binary message - string
 template<>
 void write_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_STRING>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(
-    std::cout << "DEBUG: write_member_item<string>(): " << yaml.as<std::string>() << std::endl);
+    std::cout << "DEBUG: write_member_item<string>(): " << json.as<std::string>() << std::endl);
   using CppType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_STRING>::CppType;
-  *reinterpret_cast<CppType *>(buffer) = yaml.as<std::string>();
+  *reinterpret_cast<CppType *>(buffer) = json.as<std::string>();
 }
 
 // Write an individual member into the binary message - wstring
 template<>
 void write_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_WSTRING>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(
-    std::cout << "DEBUG: write_member_item<wstring>(): " << yaml.as<std::string>() << std::endl);
+    std::cout << "DEBUG: write_member_item<wstring>(): " << json.as<std::string>() << std::endl);
   using CppType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_WSTRING>::CppType;
-  *reinterpret_cast<CppType *>(buffer) = string_to_u16string(yaml.as<std::string>());
+  *reinterpret_cast<CppType *>(buffer) = string_to_u16string(json.as<std::string>());
 }
 
 // Write an individual sequence member into the binary message - generic
 template<int RosTypeId>
 void write_sequence_member_item(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_sequence_member_item<T>(): " << std::endl);
   using CppType = typename TypeMappingCpp<RosTypeId>::CppType;
   using SequenceType = typename TypeMappingCpp<RosTypeId>::SequenceType;
   auto seq = reinterpret_cast<SequenceType *>(buffer);
-  seq->push_back(yaml.as<CppType>());
+  seq->push_back(json.as<CppType>());
 }
 
-#ifdef DYNMSG_YAML_CPP_BAD_INT8_HANDLING
+#ifdef DYNMSG_json_CPP_BAD_INT8_HANDLING
 // Write an individual sequence member into the binary message - [u]int8_t: char, octet, uint8/int8
 template<>
 void write_sequence_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_CHAR>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_sequence_member_item<char>(): " << std::endl);
   using SequenceType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_CHAR>::SequenceType;
-  std::string s = yaml.as<std::string>();
+  std::string s = json.as<std::string>();
   auto seq = reinterpret_cast<SequenceType *>(buffer);
   seq->push_back((uint8_t)std::stoul(s));
 }
 template<>
 void write_sequence_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_OCTET>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_sequence_member_item<octet>(): " << std::endl);
   using SequenceType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_OCTET>::SequenceType;
-  std::string s = yaml.as<std::string>();
+  std::string s = json.as<std::string>();
   auto seq = reinterpret_cast<SequenceType *>(buffer);
   seq->push_back((uint8_t)std::stoul(s));
 }
 template<>
 void write_sequence_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_UINT8>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_sequence_member_item<uint8>(): " << std::endl);
   using SequenceType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_UINT8>::SequenceType;
-  std::string s = yaml.as<std::string>();
+  std::string s = json.as<std::string>();
   auto seq = reinterpret_cast<SequenceType *>(buffer);
   seq->push_back((uint8_t)std::stoul(s));
 }
 template<>
 void write_sequence_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_INT8>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_sequence_member_item<int8>(): " << std::endl);
   using SequenceType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_INT8>::SequenceType;
-  std::string s = yaml.as<std::string>();
+  std::string s = json.as<std::string>();
   auto seq = reinterpret_cast<SequenceType *>(buffer);
   seq->push_back((int8_t)std::stoi(s));
 }
-#endif  // DYNMSG_YAML_CPP_BAD_INT8_HANDLING
+#endif  // DYNMSG_json_CPP_BAD_INT8_HANDLING
 
 // Write an individual sequence member into the binary message - string
 template<>
 void write_sequence_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_STRING>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(
     std::cout << "DEBUG: write_sequence_member_item<string>(): " <<
-      yaml.as<std::string>() << std::endl);
+      json.as<std::string>() << std::endl);
   using SequenceType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_STRING>::SequenceType;
   auto seq = reinterpret_cast<SequenceType *>(buffer);
-  seq->push_back(yaml.as<std::string>());
+  seq->push_back(json.as<std::string>());
 }
 
 // Write an individual sequence member into the binary message - wstring
 template<>
 void write_sequence_member_item<rosidl_typesupport_introspection_cpp::ROS_TYPE_WSTRING>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer)
 {
   DYNMSG_DEBUG(
     std::cout << "DEBUG: write_sequence_member_item<wstring>(): " <<
-      yaml.as<std::string>() << std::endl);
+      json.as<std::string>() << std::endl);
   using SequenceType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_WSTRING>::SequenceType;
   auto seq = reinterpret_cast<SequenceType *>(buffer);
-  seq->push_back(string_to_u16string(yaml.as<std::string>()));
+  seq->push_back(string_to_u16string(json.as<std::string>()));
 }
 
 // Write a sequence member into the binary message - generic
 template<int RosTypeId>
 void write_member_sequence(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer,
   const MemberInfo_Cpp & member)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_member_sequence: " << std::flush);
-  DYNMSG_DEBUG(std::cout << yaml.size() << ":" << yaml << std::endl);
-  if (member.is_upper_bound_ && yaml.size() > member.array_size_) {
-    throw std::runtime_error("yaml sequence is more than capacity");
+  DYNMSG_DEBUG(std::cout << json.size() << ":" << json << std::endl);
+  if (member.is_upper_bound_ && json.size() > member.array_size_) {
+    throw std::runtime_error("json sequence is more than capacity");
   }
-  for (size_t i = 0; i < yaml.size(); i++) {
+  for (size_t i = 0; i < json.size(); i++) {
     write_sequence_member_item<RosTypeId>(
-      yaml[i],
+      json[(int)i],
       buffer
     );
   }
@@ -373,21 +373,21 @@ void write_member_sequence(
 // https://en.cppreference.com/w/cpp/container/vector_bool
 template<>
 void write_member_sequence<rosidl_typesupport_introspection_cpp::ROS_TYPE_BOOL>(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer,
   const MemberInfo_Cpp & member)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_member_sequence<bool>: " << std::flush);
-  DYNMSG_DEBUG(std::cout << yaml.size() << ":" << yaml << std::endl);
-  if (member.is_upper_bound_ && yaml.size() > member.array_size_) {
-    throw std::runtime_error("yaml sequence is more than capacity");
+  DYNMSG_DEBUG(std::cout << json.size() << ":" << json << std::endl);
+  if (member.is_upper_bound_ && json.size() > member.array_size_) {
+    throw std::runtime_error("json sequence is more than capacity");
   }
-  // Just cast the sequence to std::vector<bool> and copy YAML node elements into it
+  // Just cast the sequence to std::vector<bool> and copy json Value elements into it
   using SequenceType =
     typename TypeMappingCpp<rosidl_typesupport_introspection_cpp::ROS_TYPE_BOOL>::SequenceType;
   auto seq = reinterpret_cast<SequenceType *>(buffer);
-  for (size_t i = 0; i < yaml.size(); i++) {
-    seq->push_back(yaml[i].as<bool>());
+  for (size_t i = 0; i < json.size(); i++) {
+    seq->push_back(json[(int)i].as<bool>());
   }
 }
 
@@ -417,9 +417,9 @@ bool is_sequence(const MemberInfo_Cpp & member)
   return false;
 }
 
-// Convert a YAML node into a field in the binary ROS message - generic
+// Convert a json Value into a field in the binary ROS message - generic
 template<int RosTypeId>
-void write_member(const YAML::Node & yaml, uint8_t * buffer, const MemberInfo_Cpp & member)
+void write_member(const Json::Value & json, uint8_t * buffer, const MemberInfo_Cpp & member)
 {
   DYNMSG_DEBUG(std::cout << "DEBUG: write_member" << std::endl);
   using CppType = typename TypeMappingCpp<RosTypeId>::CppType;
@@ -430,7 +430,7 @@ void write_member(const YAML::Node & yaml, uint8_t * buffer, const MemberInfo_Cp
 
   // Handle sequences
   if (is_sequence(member)) {
-    write_member_sequence<RosTypeId>(yaml[member.name_], buffer + member.offset_, member);
+    write_member_sequence<RosTypeId>(json[member.name_], buffer + member.offset_, member);
     return;
   }
 
@@ -438,71 +438,71 @@ void write_member(const YAML::Node & yaml, uint8_t * buffer, const MemberInfo_Cp
   if (member.is_array_) {
     for (size_t i = 0; i < member.array_size_; i++) {
       write_member_item<RosTypeId>(
-        yaml[member.name_][i],
+        json[member.name_][(int)i],
         buffer + member.offset_ + sizeof(CppType) * i
       );
     }
   } else {
     // Handle single-item members
-    write_member_item<RosTypeId>(yaml[member.name_], buffer + member.offset_);
+    write_member_item<RosTypeId>(json[member.name_], buffer + member.offset_);
   }
 }
 
-// Convert a nested YAML sequence node
+// Convert a nested json sequence Value
 void write_member_sequence_nested(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer,
   const MemberInfo_Cpp & member)
 {
-  if (member.is_upper_bound_ && yaml.size() > member.array_size_) {
-    throw std::runtime_error("yaml sequence is more than capacity");
+  if (member.is_upper_bound_ && json.size() > member.array_size_) {
+    throw std::runtime_error("json sequence is more than capacity");
   }
   const TypeInfo_Cpp * member_typeinfo =
     reinterpret_cast<const TypeInfo_Cpp *>(member.members_->data);
   auto & seq = buffer;
-  member.resize_function(seq, yaml.size());
-  for (size_t i = 0; i < yaml.size(); i++) {
-    yaml_to_rosmsg_impl(
-      yaml[i],
+  member.resize_function(seq, json.size());
+  for (size_t i = 0; i < json.size(); i++) {
+    json_to_rosmsg_impl(
+      json[(int)i],
       member_typeinfo,
       reinterpret_cast<uint8_t *>(member.get_function(seq, i))
     );
   }
 }
 
-// Convert a nested YAML node
+// Convert a nested json Value
 void write_member_nested(
-  const YAML::Node & yaml,
+  const Json::Value & json,
   uint8_t * buffer,
   const MemberInfo_Cpp & member)
 {
   if (is_sequence(member)) {
-    write_member_sequence_nested(yaml[member.name_], buffer + member.offset_, member);
+    write_member_sequence_nested(json[member.name_], buffer + member.offset_, member);
     return;
   }
 
   const TypeInfo_Cpp * member_typeinfo =
     reinterpret_cast<const TypeInfo_Cpp *>(member.members_->data);
   if (member.is_array_) {
-    for (size_t i = 0; i < yaml[member.name_].size(); i++) {
-      yaml_to_rosmsg_impl(
-        yaml[member.name_][i],
+    for (size_t i = 0; i < json[member.name_].size(); i++) {
+      json_to_rosmsg_impl(
+        json[member.name_][(int)i],
         member_typeinfo, buffer + member.offset_ + member_typeinfo->size_of_ * i
       );
     }
   } else {
-    yaml_to_rosmsg_impl(yaml[member.name_], member_typeinfo, buffer + member.offset_);
+    json_to_rosmsg_impl(json[member.name_], member_typeinfo, buffer + member.offset_);
   }
 }
 
-// Convert a YAML representation to a binary ROS message by looping over the expected fields of the
-// ROS message and getting their values from the YAML
-void yaml_to_rosmsg_impl(
-  const YAML::Node & root,
+// Convert a json representation to a binary ROS message by looping over the expected fields of the
+// ROS message and getting their values from the json
+void json_to_rosmsg_impl(
+  const Json::Value & root,
   const TypeInfo_Cpp * typeinfo,
   uint8_t * buffer)
 {
-  DYNMSG_DEBUG(std::cout << "DEBUG: yaml_to_rosmsg_impl" << std::endl);
+  DYNMSG_DEBUG(std::cout << "DEBUG: json_to_rosmsg_impl" << std::endl);
   DYNMSG_DEBUG(
     std::cout << "DEBUG: type_info message_namespace_: " <<
       typeinfo->message_namespace_ << std::endl);
@@ -579,21 +579,21 @@ void yaml_to_rosmsg_impl(
 
 }  // namespace impl
 
-void yaml_and_typeinfo_to_rosmsg(
+void json_and_typeinfo_to_rosmsg(
   const TypeInfo_Cpp * type_info,
-  const std::string & yaml_str,
+  const std::string & json_str,
   void * ros_message)
 {
-  // Parse the YAML representation to an in-memory representation
-  YAML::Node root = YAML::Load(yaml_str);
-  // Convert the YAML representation to a binary representation
+  // Parse the json representation to an in-memory representation
+  const Json::Value root(json_str);
+  // Convert the json representation to a binary representation
   uint8_t * buffer = reinterpret_cast<uint8_t *>(ros_message);
-  impl::yaml_to_rosmsg_impl(root, type_info, buffer);
+  impl::json_to_rosmsg_impl(root, type_info, buffer);
 }
 
-RosMessage_Cpp yaml_and_typeinfo_to_rosmsg(
+RosMessage_Cpp json_and_typeinfo_to_rosmsg(
   const TypeInfo_Cpp * type_info,
-  const std::string & yaml_str,
+  const std::string & json_str,
   rcutils_allocator_t * allocator)
 {
   rcutils_allocator_t default_allocator = rcutils_get_default_allocator();
@@ -608,20 +608,20 @@ RosMessage_Cpp yaml_and_typeinfo_to_rosmsg(
   {
     return {nullptr, nullptr};
   }
-  yaml_and_typeinfo_to_rosmsg(type_info, yaml_str, reinterpret_cast<void *>(ros_msg.data));
+  json_and_typeinfo_to_rosmsg(type_info, json_str, reinterpret_cast<void *>(ros_msg.data));
   return ros_msg;
 }
 
-RosMessage_Cpp yaml_to_rosmsg(
+RosMessage_Cpp json_to_rosmsg(
   const InterfaceTypeName & interface_type,
-  const std::string & yaml_str)
+  const std::string & json_str)
 {
   const auto * type_info = dynmsg::cpp::get_type_info(interface_type);
   if (nullptr == type_info) {
     return {nullptr, nullptr};
   }
   rcutils_allocator_t * allocator = nullptr;
-  return yaml_and_typeinfo_to_rosmsg(type_info, yaml_str, allocator);
+  return json_and_typeinfo_to_rosmsg(type_info, json_str, allocator);
 }
 
 }  // namespace cpp
